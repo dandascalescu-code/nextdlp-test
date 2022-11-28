c430699dc054871023cdd87d595e5fba85a7da36
ddascalescu <djdascalescu@gmail.com>
2022-11-28 19:07:55 +0000

    Added cpp test and comments

--- files/cpp/MyClass.cpp
+++ files/cpp/MyClass.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 
+/// @brief Class to demonstrate tracking number of object instances
 class MyClass {
-    static int nobjs;
+    static int numobjs;
 
-    MyClass() {
-        nobjs++;
+    public: MyClass() {
+        numobjs++;
     }
 
-    ~MyClass() {
-        nobjs--;
+    public: ~MyClass() {
+        numobjs--;
     }
 
+    /// @return number of MyClass objects currently in existance
     public: static int nobjs() {
-        return nobjs;
+        return numobjs;
     }
 };
+int MyClass::numobjs = 0;
 
-int MyClass::nobjs = 0;
+/* The following is a test of the MyClass functionality */
+
+void printn() {
+    std::cout 
+        << "\nNumber of objects: "
+        << MyClass::nobjs();
+}
 
 main() {
-    MyClass a, b, c;
-    cout 
-        << "Number of objects: "
-        << MyClass::nobjs()
+    printn(); // expect 0
+    {
+        MyClass a, b, c;
+        printn(); // expect 3
+        {
+            MyClass d, e, f;
+            printn(); // expect 6
+        }
+        printn(); // expect 3
+    }
+    printn(); // expect 0
 }
+
+/* End of test */
