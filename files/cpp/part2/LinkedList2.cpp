dde604159c3042e41f2f7d34d609ddda8062bb13
ddascalescu <djdascalescu@gmail.com>
2022-12-01 14:32:04 +0000

    Updated cpp LinkedList2

--- files/cpp/part2/LinkedList2.cpp
+++ files/cpp/part2/LinkedList2.cpp
@@ -17,7 +17,41 @@ std::ostream& operator<<(std::ostream& os, const node_t& node) {
 };
 
 node_t* even_nodes(node_t** ptr) {
-    return nullptr;
+    node_t* headptr = *ptr;
+    node_t* currentptr = headptr;
+    node_t* prevptr = nullptr;
+
+    node_t* new_headptr = nullptr;
+    node_t* new_currentptr = new_headptr;
+
+    while (currentptr != nullptr) {
+        node_t current = *currentptr;
+        if (current.v % 2 == 0) {
+            // remove from given list
+            if (prevptr == nullptr)
+                headptr = current.next;
+            else {
+                node_t previous = *prevptr;
+                previous.next = current.next;
+            }
+
+            // add to new list
+            if (new_currentptr == nullptr)
+                new_headptr = currentptr;
+            else {
+                node_t new_current = *new_currentptr;
+                new_current.next = currentptr;
+            }
+
+            //update newlist pointer
+            new_currentptr = currentptr;
+        }
+
+        //update pointer
+        currentptr = current.next;
+    }
+
+    return new_headptr;
 };
 
 main() {
